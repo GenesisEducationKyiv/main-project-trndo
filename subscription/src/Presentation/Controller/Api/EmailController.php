b8bddf6987664da63d8802b2f74b47f952214f68
trndo <trndogv@gmail.com>
2023-07-15 00:47:13 +0300

    hw-7: update and fix subscription ms

--- subscription/src/Presentation/Controller/Api/EmailController.php
+++ subscription/src/Presentation/Controller/Api/EmailController.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/api')]
-class CurrencyRateEmailController
+class EmailController
 {
     public function __construct(
         private CurrencyClientInterface $rateClient,
